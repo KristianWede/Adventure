c5df85c7ceeed6ba1b42b693f75baab2cc71b372
KristianWede <kris79s4@stud.kea.dk>
2022-03-14 14:06:15 +0100

    Added userInput (Prototype)

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -1,12 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Adventure {
+
+    public String userInput(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your next move?");
+        System.out.println("Type 'help' for help.");
+        String descesion = sc.nextLine();
+
+        return descesion;
+    }
+
 
     public static void main(String[] args) {
         // Part 1: The room.
         int currentRoom = 1;
-        Room room = new Room();
+        Room room1 = new Room();
+        Adventure main = new Adventure();
+
+
 
         System.out.println("This code works:");
+
+
+
     }
 }
