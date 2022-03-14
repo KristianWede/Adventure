db5191e291c216da148efe345ae8d3d47c8e5e51
Ramilade <rami0904@stud.kea.dk>
2022-03-14 14:47:25 +0100

    merge med kristian

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -3,7 +3,7 @@ package com.company;
 public class Room {
   private String name;
   private String description;
-  private Room n,e,s, w;
+  private Room n, e, s, w;
 
   public Room(String name, String description, Room n, Room e, Room s, Room w) {
     this.name = name;
@@ -14,17 +14,37 @@ public class Room {
     this.w = w;
   }
 
-
-
-  /*public Room(String name, String description){
-
-
-    this.name = name;
-    this.description = description;
-    this.n = n;
-    this.e = e;
-    this.s =
-*/
+  public Room getN() {
+    return n;
   }
+
+  public void setN(Room n) {
+    this.n = n;
+  }
+
+  public void setE(Room e) {
+    this.e = e;
+  }
+
+  public Room getE() {
+    return e;
+  }
+
+  public Room getS() {
+    return s;
+  }
+
+  public void setS(Room s) {
+    this.s = s;
+  }
+
+  public Room getW() {
+    return w;
+  }
+
+  public void setW(Room w) {
+    this.w = w;
+  }
+}
 
 
