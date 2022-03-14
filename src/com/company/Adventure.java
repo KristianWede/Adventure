e73a316b340bfa9f41be6c8a46722c1ba877b54a
KristianWede <kris79s4@stud.kea.dk>
2022-03-14 13:39:43 +0100

    First entry, no function.

---
+++ src/com/company/Adventure.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Adventure {
+
+    public static void main(String[] args) {
+        // Part 1: Rummene.
+        int currentRoom = 1;
+        Rum room = new Rum();
+
+        System.out.println("Denne kode virker:");
+    }
+}
