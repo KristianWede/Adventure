04349e24d1c4b0523daf4b9c28fe9369ae9de232
Ramilade <rami0904@stud.kea.dk>
2022-03-14 15:27:33 +0100

    merge med kristian

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Adventure {
     public void worldMap() {
-        Room room1 = new Room("Room1", "Something something story here");
-        Room room2 = new Room("Room2", "Something something story here");
-        Room room3 = new Room("Room3", "Something something story here");
-        Room room4 = new Room("Room4", "Something something story here");
-        Room room5 = new Room("Room5", "Something something story here");
-        Room room6 = new Room("Room6", "Something something story here");
-        Room room7 = new Room("Room7", "Something something story here");
-        Room room8 = new Room("Room8", "Something something story here");
-        Room room9 = new Room("Room9", "Something something story here");
+        Room room1 = new Room("Room1", "This is room 1");
+        Room room2 = new Room("Room2", "This is room 2");
+        Room room3 = new Room("Room3", "This is room 3");
+        Room room4 = new Room("Room4", "This is room 4");
+        Room room5 = new Room("Room5", "This is room 5");
+        Room room6 = new Room("Room6", "This is room 6");
+        Room room7 = new Room("Room7", "This is room 7");
+        Room room8 = new Room("Room8", "This is room 8");
+        Room room9 = new Room("Room9", "This is room 9");
 
         //Maps out what "fields" are available to each other, use the map for reference.
         //Room 1
