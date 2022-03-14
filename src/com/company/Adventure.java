7fe8dcd250f50185fd6d0a11ba73ffe9d7a4187e
KristianWede <kris79s4@stud.kea.dk>
2022-03-14 14:27:14 +0100

    Added 1. (userInput)

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -1,27 +1,76 @@
 package com.company;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Adventure {
+
+    public void execute() {
+        // Part 1: The room.
+        int currentRoom = 1;
+        //Room room1 = new Room();
+        System.out.println("""
+                En meget lang
+                beskriverlse på
+                adskillige linjer""");
+
+        recieveInputandGiveOutPut(userInput());
+    }
+
+    public void recieveInputandGiveOutPut(String input){
+
+
+
+        switch (input){
+            case "go north":
+                System.out.println("Going North.");
+                break;
+
+            case "go south":
+                System.out.println("Going South.");
+                break;
+
+            case "go east":
+                System.out.println("Going East.");
+                break;
+
+            case "go west":
+                System.out.println("Going West.");
+                break;
+
+            case "look":
+                System.out.println("Looking around.");
+                break;
+
+            case "help":
+                System.out.println("You can say cheese.");
+                break;
+
+            case "exit":
+                System.out.println("Exiting game...");
+                System.out.println("Hope you saved your progress, cause I didn't!");
+                System.exit(0);
+                break;
+
+            default:
+                System.out.println("Not a valid choice!");
+        }
+
+    }
+
 
     public String userInput(){
         Scanner sc = new Scanner(System.in);
         System.out.println("What is your next move?");
         System.out.println("Type 'help' for help.");
-        String descesion = sc.nextLine();
+        String descesion = sc.nextLine().toLowerCase();
 
         return descesion;
     }
 
 
     public static void main(String[] args) {
-        // Part 1: The room.
-        int currentRoom = 1;
-        Room room1 = new Room();
-        Adventure main = new Adventure();
-
-
-
-        System.out.println("This code works:");
+        Adventure obj = new Adventure();
+        obj.execute();
     }
 }
