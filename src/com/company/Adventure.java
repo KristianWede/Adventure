b48712a3d6347e8263c749ea893c76c1b0d49375
KristianWede <kris79s4@stud.kea.dk>
2022-03-14 14:36:57 +0100

    Added 1. (userInput) for real

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -9,63 +9,53 @@ public class Adventure {
         // Part 1: The room.
         int currentRoom = 1;
         //Room room1 = new Room();
-        System.out.println("""
-                En meget lang
-                beskriverlse på
-                adskillige linjer""");
-
-        recieveInputandGiveOutPut(userInput());
+        System.out.println("Dit nuværende rum er " + currentRoom);
+        userInput();
     }
-
-    public void recieveInputandGiveOutPut(String input){
-
-
-
-        switch (input){
-            case "go north":
-                System.out.println("Going North.");
-                break;
-
-            case "go south":
-                System.out.println("Going South.");
-                break;
-
-            case "go east":
-                System.out.println("Going East.");
-                break;
-
-            case "go west":
-                System.out.println("Going West.");
-                break;
-
-            case "look":
-                System.out.println("Looking around.");
-                break;
-
-            case "help":
-                System.out.println("You can say cheese.");
-                break;
-
-            case "exit":
-                System.out.println("Exiting game...");
-                System.out.println("Hope you saved your progress, cause I didn't!");
-                System.exit(0);
-                break;
-
-            default:
-                System.out.println("Not a valid choice!");
-        }
-
-    }
-
 
     public String userInput(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("What is your next move?");
-        System.out.println("Type 'help' for help.");
-        String descesion = sc.nextLine().toLowerCase();
+        do{
+            System.out.println("What is your next move?");
+            System.out.println("Type 'help' for help.");
+            String descesion = sc.nextLine().toLowerCase();
+            switch (descesion) {
+                case "go north":
+                    System.out.println("Going North.");
+                    break;
 
-        return descesion;
+                case "go south":
+                    System.out.println("Going South.");
+                    break;
+
+                case "go east":
+                    System.out.println("Going East.");
+                    break;
+
+                case "go west":
+                    System.out.println("Going West.");
+                    break;
+
+                case "look":
+                    System.out.println("Looking around.");
+                    break;
+
+                case "help":
+                    System.out.println("You can say cheese.");
+                    break;
+
+                case "exit":
+                    System.out.println("Exiting game...");
+                    System.out.println("Hope you saved your progress, cause I didn't!");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Not a valid choice!");
+                    break;
+            }
+        } while(true);
+
     }
 
 
