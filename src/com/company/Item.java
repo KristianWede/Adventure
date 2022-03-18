6111b4078dbe49b286be399ea17db4e8711500b8
Ramilade <rami0904@stud.kea.dk>
2022-03-18 12:39:32 +0100

    Musik tilføjet

---
+++ src/com/company/Item.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Item {
+
+  public String itemName;
+  public String itemDescription;
+
+  public Item(String itemName, String itemDescription){
+    this.itemName = itemName;
+    this.itemDescription = "This is an Item Description";
+  }
+
+  public String getItemName(){
+    return itemName;
+  }
+  public String getItemDescription(){
+    return itemDescription;
+  }
+
+  public String toString(){
+    return itemName;
+  }
+
+}
+
